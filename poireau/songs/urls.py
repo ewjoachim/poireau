e56e98ad253f5051b2d853018dacba136be6b1d4
Joachim Jablon <ewjoachim@gmail.com>
2015-01-25 10:46:38 +0100

    Detail page that shows first notes + random view

--- poireau/songs/urls.py
+++ poireau/songs/urls.py
@@ -8,4 +8,6 @@ from .views import song
 urlpatterns = patterns(
     '',
     url(r'^song$', song.SongListView.as_view(), name='song_list'),
+    url(r'^song/(?P<pk>\d+)$', song.SongDetailView.as_view(), name='song_detail'),
+    url(r'^song/random$', song.SongRandomView.as_view(), name='song_random'),
 )
--- poireau/songs/views/song.py
+++ poireau/songs/views/song.py
@@ -1,6 +1,8 @@
 from __future__ import unicode_literals
 
-from django.views.generic import ListView
+from django.views.generic import View, ListView, DetailView
+from django import http
+from django.core.urlresolvers import reverse_lazy as reverse
 
 from poireau.common.views import BaseViewMixin
 from poireau.songs.models import Song
@@ -16,3 +18,18 @@ class SongListView(SongMixin, ListView):
 
     def get_queryset(self):
         return super(SongListView, self).get_queryset().order_by("path")
+
+
+class SongDetailView(SongMixin, DetailView):
+    template_name = "songs/detail.html"
+    menu_list = ["songs"]
+
+
+class SongRandomView(SongMixin, View):
+
+    def get(self, request):
+        song = Song.objects.order_by("?").first()
+        if song:
+            return http.HttpResponseRedirect(song.get_absolute_url())
+        else:
+            return http.HttpResponseRedirect(reverse("song_list"))
