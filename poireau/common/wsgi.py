62eb4115430d8858517946b8c3782b799f7f8d68
Joachim Jablon <ewjoachim@gmail.com>
2015-05-11 00:14:18 +0200

    Static files serving in production

--- poireau/common/wsgi.py
+++ poireau/common/wsgi.py
@@ -8,8 +8,9 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "poireau.common.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
